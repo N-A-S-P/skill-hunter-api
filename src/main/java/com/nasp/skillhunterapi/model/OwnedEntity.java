8d10487f32240c506c1c3e19bd89ffc0f42af446
Kristen Nielsen <kristen.e.nielsen@gmail.com>
2026-06-03 21:51:52 -0500

    Added `@Getter` and `@Setter` to the abstract classes

--- src/main/java/com/nasp/skillhunterapi/model/OwnedEntity.java
+++ src/main/java/com/nasp/skillhunterapi/model/OwnedEntity.java
@@ -3,9 +3,13 @@ package com.nasp.skillhunterapi.model;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
 
 @MappedSuperclass
 public abstract class OwnedEntity extends BaseEntity {
+    @Getter
+    @Setter
     @ManyToOne
     @JoinColumn(name = "owner_id", nullable = false)
     protected AppUser owner;
