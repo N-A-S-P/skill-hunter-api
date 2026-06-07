c84c4f36f6d3efd6817dcf221f7dae93b86ed30f
Kristen Nielsen <kristen.e.nielsen@gmail.com>
2026-06-07 01:19:22 -0500

    Database updates and added keycloak to Docker Compose.

--- src/main/java/com/nasp/skillhunterapi/model/CompanyContactRelationship.java
+++ src/main/java/com/nasp/skillhunterapi/model/CompanyContactRelationship.java
@@ -2,10 +2,20 @@ package com.nasp.skillhunterapi.model;
 
 import com.nasp.skillhunterapi.enums.RelationshipType;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "company_contact_relationships")
-public class CompanyContactRelationship extends BaseEntity{
+public class CompanyContactRelationship extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "contact_id", nullable = false)
     private Contact contact;
@@ -13,8 +23,14 @@ public class CompanyContactRelationship extends BaseEntity{
     @ManyToOne
     @JoinColumn(name = "company_id", nullable = false)
     private Company company;
+    private Boolean isCompanyEmployee;
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private RelationshipType relationshipType;
+    private Boolean isCurrentRelationship;
+
+    private LocalDateTime startedOn;
+    private LocalDateTime endedOn;
+
 }
