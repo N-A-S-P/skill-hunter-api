0168d0ac8fb3a8f8d02b610a6ba34ceeeee4be2e
Kristen Nielsen <kristen.e.nielsen@gmail.com>
2026-06-09 01:56:04 -0500

    Addresses are now Company's problem

--- src/main/java/com/nasp/skillhunterapi/model/Company.java
+++ src/main/java/com/nasp/skillhunterapi/model/Company.java
@@ -2,38 +2,72 @@ package com.nasp.skillhunterapi.model;
 
 import com.nasp.skillhunterapi.enums.CompanyType;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "companies")
 public class Company extends OwnedEntity {
+    public Company() {
+        this.companyTypes = Set.of();
+        this.addresses = new ArrayList<>();
+    }
+    public Company(String name, String website, String industry, Set<CompanyType> companyTypes, List<Address> addresses) {
+        this.name = name;
+        this.website = website;
+        this.industry = industry;
+        this.companyTypes = companyTypes == null ? Set.of() : companyTypes;
+        this.addresses = new ArrayList<>();
+
+        if (addresses != null) {
+            addresses.forEach(this::addAddress);
+        }
+    }
+
+    @NotBlank
     private String name;
     private String website;
     private String industry;
 
-    @OneToMany(mappedBy="company", cascade = CascadeType.ALL, orphanRemoval = true)
+    @ElementCollection
+    @Enumerated(EnumType.STRING)
+    private Set<CompanyType> companyTypes;
+
+    @OneToMany(mappedBy = "company", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Address> addresses;
 
     @OneToMany(mappedBy = "company", cascade = CascadeType.ALL)
     private List<CompanyContactRelationship> contactRelationships;
 
-    @OneToMany(mappedBy = "company", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "company", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Position> positions;
 
     @OneToMany(mappedBy = "employerCompany", cascade = CascadeType.ALL)
     private List<Placement> placements;
 
-    @ElementCollection
-    @Enumerated(EnumType.STRING)
-    private Set<CompanyType> companyTypes;
+    public void addAddress(@NotNull Address address) {
+        Objects.requireNonNull(address, "address must not be null");
+        address.setCompany(this);
+        addresses.add(address);
+    }
+
+    public Optional<Address> getAddressById(Long addressId) {
+        return addresses.stream().filter(address -> address.getId().equals(addressId)).findFirst();
+    }
+
+    public void removeAddressById(Long addressId) {
+        var optionalAddress = getAddressById(addressId);
+
+        if (optionalAddress.isPresent()) {
+            var address = optionalAddress.get();
+            addresses.remove(address);
+            address.setCompany(null);
+        }
+    }
 }
---
+++ src/test/java/com/nasp/skillhunterapi/model/CompanyTests.java
@@ -0,0 +1,141 @@
+package com.nasp.skillhunterapi.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CompanyTests {
+    @Test
+    @DisplayName("No Parameter constructor should initialize relationships")
+    void constructor_noParams() {
+        var company = new Company();
+
+        assertThat(company.getCompanyTypes()).isNotNull();
+
+        assertThat(company.getAddresses()).isNotNull();
+        assertThat(company.getAddresses()).hasSize(0);
+    }
+
+    @Nested
+    @DisplayName("Initialization with passed addresses")
+    class ConstructorWithAddressRelationshipInitialization {
+        @Test
+        @DisplayName("should set the parent of each address passed in")
+        void constructor_addressesIncluded_addressRelationshipNotNull() {
+            var address = new Address();
+            var company = new Company(
+                    "Vizzini's Army",
+                    "https://www.inconceivable.com",
+                    "Mercenaries",
+                    Set.of(),
+                    List.of(address));
+
+            assertThat(company.getAddresses()).isNotNull();
+            assertThat(company.getAddresses()).hasSize(1);
+
+            assertThat(address.getCompany()).isEqualTo(company);
+        }
+
+        @Test
+        @DisplayName("should initialize address relationships if not passed any")
+        void constructor_addressesIncluded_addressesNull() {
+            var company = new Company(
+                    "Vizzini's Army",
+                    "https://www.inconceivable.com",
+                    "Mercenaries",
+                    Set.of(),
+                    null);
+
+            assertThat(company.getAddresses()).isNotNull();
+            assertThat(company.getAddresses()).hasSize(0);
+        }
+    }
+
+    @Nested
+    @DisplayName("addAddress")
+    class AddAddress {
+        @Test
+        @DisplayName("should add an address")
+        void addAddress_happyPath() {
+            var address = new Address();
+            var company = new Company();
+
+            company.addAddress(address);
+
+            assertThat(company.getAddresses()).contains(address);
+            assertThat(address.getCompany()).isEqualTo(company);
+        }
+
+        @Test
+        @DisplayName("should throw exception when address is null")
+        void addAddress_nullAddress() {
+            var company = new Company();
+
+            assertThatThrownBy(() -> company.addAddress(null))
+                    .isInstanceOf(NullPointerException.class)
+                    .hasMessage("address must not be null");
+        }
+    }
+
+    @Nested
+    @DisplayName("getAddressById")
+    class GetAddressById {
+        @Test
+        @DisplayName("should return address if exists")
+        void happyPath() {
+            var address = new Address();
+            address.setId(1L);
+
+            var company = new Company("name", "", "", Set.of(), List.of(address));
+
+            var result = company.getAddressById(1L);
+
+            assertThat(result).contains(address);
+        }
+
+        @Test
+        @DisplayName("should return empty Optional if no matching address")
+        void notExists() {
+            var company = new Company();
+
+            var result = company.getAddressById(1L);
+
+            assertThat(result).isEmpty();
+        }
+    }
+
+    @Nested
+    @DisplayName("removeAddressById")
+    class RemoveAddressById {
+        @Test
+        @DisplayName("should remove address")
+        void happyPath() {
+            var address = new Address();
+            address.setId(1L);
+            var company = new Company("Magic Max's Marvelous Market", "", "Retail", Set.of(), List.of(address));
+
+            company.removeAddressById(1L);
+
+            assertThat(company.getAddresses()).doesNotContain(address);
+            assertThat(address.getCompany()).isNull();
+        }
+        @Test
+        @DisplayName("should do nothing")
+        void noMatch() {
+            var address = new Address();
+            address.setId(1L);
+            var company = new Company("Magic Max's Marvelous Market", "", "Retail", Set.of(), List.of(address));
+
+            company.removeAddressById(2L);
+
+            assertThat(company.getAddresses()).contains(address);
+            assertThat(address.getCompany()).isNull();
+        }
+    }
+}
