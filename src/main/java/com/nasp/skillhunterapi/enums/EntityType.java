6862b27068768767a6637ce38f28779ec8e12cdb
Kristen Nielsen <kristen.e.nielsen@gmail.com>
2026-06-09 18:28:34 -0500

    Added JaCoCo for coverage

--- src/main/java/com/nasp/skillhunterapi/enums/EntityType.java
+++ src/main/java/com/nasp/skillhunterapi/enums/EntityType.java
@@ -1,21 +1,10 @@
 package com.nasp.skillhunterapi.enums;
 
-public enum EntityType implements LookupEnum {
-    COMPANY("Company"),
-    CONTACT("Contact"),
-    INTERACTION("Interaction"),
-    PLACEMENT("Placement"),
-    POSITION("Position"),
-    POSITION_APPLICATION("Application");
-
-    private final String display;
-
-    EntityType(String display) {
-        this.display = display;
-    }
-
-    @Override
-    public String getDisplay() {
-        return display;
-    }
+public enum EntityType {
+    COMPANY,
+    CONTACT,
+    INTERACTION,
+    PLACEMENT,
+    POSITION,
+    POSITION_APPLICATION
 }
